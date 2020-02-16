1c317f274f315743793ad04f4720611472b15b22
aldyerifianzah <aldyerifianzah.edu@gmail.com>
2020-02-16 16:27:46 +0700

    tugas graduated

---
+++ graduate.js
@@ -0,0 +1,120 @@
+function graduates(students) {
+    // Code disini
+
+    var hasil = {}
+
+    if (students.length == 0) {
+
+        return hasil;
+    }
+
+    else {
+
+        //buat label
+
+        for (let i = 0; i < students.length; i++) {
+            hasil[students[i].class] = [];
+
+        }
+
+        //membuat value di label
+
+        for (let j = 0; j < students.length; j++) {
+
+            var tempt = {
+
+                name: '',
+                score: 0
+            }
+
+            //pemasukan nilai
+            if (students[j].score > 75) {
+
+                tempt.name = students[j].name;
+                tempt.score = students[j].score;
+
+                hasil[students[j].class].push(tempt)
+            }
+
+        }
+
+        return hasil
+    }
+}
+
+console.log(graduates([
+    {
+        name: 'Dimitri',
+        score: 90,
+        class: 'foxes'
+    },
+    {
+        name: 'Alexei',
+        score: 85,
+        class: 'wolves'
+    },
+    {
+        name: 'Sergei',
+        score: 74,
+        class: 'foxes'
+    },
+    {
+        name: 'Anastasia',
+        score: 78,
+        class: 'wolves'
+    }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+    {
+        name: 'Alexander',
+        score: 100,
+        class: 'foxes'
+    },
+    {
+        name: 'Alisa',
+        score: 76,
+        class: 'wolves'
+    },
+    {
+        name: 'Vladimir',
+        score: 92,
+        class: 'foxes'
+    },
+    {
+        name: 'Albert',
+        score: 71,
+        class: 'wolves'
+    },
+    {
+        name: 'Viktor',
+        score: 80,
+        class: 'tigers'
+    }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
